bd9377b41e1f4f3207b5c0e429b38ba3c6ccb90c
matbujnowicz <matbujnowicz@gmail.com>
2021-10-17 20:01:26 +0200

    Add new assets

--- src/data.js
+++ src/data.js
@@ -28,6 +28,76 @@ const data = [
         title: "Szymi wujas na weselu",
         description: "Ze mna sie mlody nie napijesz???",
         src: "wujas.png"
+    },
+    {
+        title: "Szymi atleta",
+        description: "Giętki jak mało ktory",
+        src: "atleta.jpeg"
+    },
+    {
+        title: "Szymi blachara",
+        description: "sika na widok golfa 3",
+        src: "blachara.jpeg"
+    },
+    {
+        title: "Szymi paker",
+        description: "siłacz jakich mało. sylwetka adonisa",
+        src: "dojebany.png"
+    },
+    {
+        title: "Szymi elegancki",
+        description: "w sam raz na komunie/wesele/pogrzeb",
+        src: "elegant.jpeg"
+    },
+    {
+        title: "Szymi gotowy na jebanie",
+        description: "owiniety -> nie peknie",
+        src: "gotowy.jpeg"
+    },
+    {
+        title: "Szymi jajcarz",
+        description: "Co? Jajecczko :==)",
+        src: "jajcarz.jpeg"
+    },
+    {
+        title: "Szymi ogniskowy",
+        description: "Ogrzeje ci kiełbe",
+        src: "kielba.jpeg"
+    },
+    {
+        title: "Szymi stylowy fryz",
+        description: "Kto się oprze takiej kiteczce",
+        src: "kitka.jpeg"
+    },
+    {
+        title: "Szymi lodziarz",
+        description: "Juz wiemy skad ksywa ALgidziak :PP",
+        src: "lodzik.png"
+    },
+    {
+        title: "Szymi raper",
+        description: "wytrzep w koncu ten jebaany dywan - AL",
+        src: "raper.jpeg"
+    },
+    {
+        title: "Szymi zrelaksowany",
+        description: "nic dodac nic ujac",
+        src: "relaks.png"
+    },
+    {
+        title: "Szymi jebaka",
+        description: "ma wielkiego ptaka",
+        src: "ruhacz.png"
+    },
+    {
+        title: "Szymi stylowy",
+        description: "Trendsetter, polska nie gotowa na szymiryczke",
+        src: "stylowy.jpeg"
+    },
+    {
+        title: "Szymi ziomal",
+        description: "Nie rozumiesz mnie? Mam cie gdzieeś",
+        src: "ziomal.jpeg"
     }
 ]
 
